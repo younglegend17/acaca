a63a6e6e2bd4710d39556021e9747119441a2cec
Саша <pinpinych@mail.ru>
2023-07-03 23:22:40 +0500

    аааа

---
+++ provertepozhaluista.py
@@ -0,0 +1,14 @@
+def is_pal(s):
+
+    reverse = s[::-1]
+    rev = reverse
+
+    # проверяем, совпадают ли строки
+    if (s == rev):
+        return True
+    else:
+        return False
+
+
+b = str(input())
+print(is_pal(b))
